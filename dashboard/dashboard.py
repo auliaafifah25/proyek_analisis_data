5d63bc6fb039ab12d774e7c0d65def86abcc0334
Au Lia <aulia2203lia@gmail.com>
2024-05-04 15:31:01 +0700

    hapus import

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
 from babel.numbers import format_currency
 sns.set(style='dark')
 
