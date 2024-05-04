2b34524400a37fde5faadc33b736f9c272b1783c
Au Lia <aulia2203lia@gmail.com>
2024-05-04 12:44:48 +0700

    tambah folder&file

---
+++ dashboard/dashboard.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+
+
